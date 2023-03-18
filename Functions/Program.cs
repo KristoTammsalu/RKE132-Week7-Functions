3c58bf1d382f55bd68344c854e0260bd0b9f3d09
Kristi Tammsalu <kristi.tammsalu@metalexpress.local>
2023-03-18 14:40:12 +0200

    create new app

---
+++ Functions/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Are you coming or leaving? (in/out):");
+string userChoice = Console.ReadLine();
+
+
+if(userChoice == "in")
+{
+    PrintHello();
+}
+else
+{
+    printGoodBye();
+}
+
+
+
+
+
+static void PrintHello() //function/ method
+{
+    Console.WriteLine("Hello world!");
+}
+
+static void printGoodBye()
+{
+    Console.WriteLine("See you later, alligator.");
+}
